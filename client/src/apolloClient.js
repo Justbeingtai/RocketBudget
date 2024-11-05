6a4b913a8b44f5b949225ac7ddbb33e575342c27
Justbeingtai <taiho909pro@gmail.com>
2024-11-04 22:03:13 -0600

    Set up Apollo Client for GraphQL in apolloClient.js

--- client/src/apolloClient.js
+++ client/src/apolloClient.js
@@ -1,7 +1,8 @@
+// client/src/apolloClient.js
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: 'http://localhost:4000/graphql', // This URL will point to the GraphQL server
   cache: new InMemoryCache(),
 });
 
