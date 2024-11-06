0bf8f726fdfd99e06ba2f2ad1247ea07072bd4b2
Justbeingtai <taiho909pro@gmail.com>
2024-11-05 20:42:45 -0600

    Add mutations for income and expense in queries.js

--- client/src/queries.js
+++ client/src/queries.js
@@ -1,4 +1,3 @@
-// client/src/queries.js
 import { gql } from '@apollo/client';
 
 export const GET_INCOMES = gql`
@@ -15,6 +14,30 @@ export const GET_INCOMES = gql`
 export const GET_EXPENSES = gql`
   query GetExpenses {
     expenses {
+      _id
+      amount
+      category
+      date
+    }
+  }
+`;
+
+// Mutation to add income
+export const ADD_INCOME = gql`
+  mutation AddIncome($amount: Float!, $source: String!, $date: String!) {
+    addIncome(amount: $amount, source: $source, date: $date) {
+      _id
+      amount
+      source
+      date
+    }
+  }
+`;
+
+// Mutation to add expense
+export const ADD_EXPENSE = gql`
+  mutation AddExpense($amount: Float!, $category: String!, $date: String!) {
+    addExpense(amount: $amount, category: $category, date: $date) {
       _id
       amount
       category
