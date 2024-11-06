d091c470e3fdae801785ae4396e829e071f343b9
Justbeingtai <taiho909pro@gmail.com>
2024-11-05 23:36:08 -0600

    made server avail everywhere on mongodb

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,13 +4,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const connectOptions = {
+  tls: true,
+  tlsAllowInvalidCertificates: true,
+};
+
+mongoose.connect(process.env.MONGODB_URI, connectOptions);
 
 const db = mongoose.connection;
-
 db.on('error', (error) => console.error('MongoDB connection error:', error));
 db.once('open', () => {
   console.log('Connected to MongoDB');
