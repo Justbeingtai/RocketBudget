65c1b84058704fb75aa012b70e302fd1eba443da
Justbeingtai <taiho909pro@gmail.com>
2024-11-06 00:29:02 -0600

    render update 10

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    define: {
-      'process.env.VITE_GRAPHQL_URI': mode === 'production'
-        ? '"https://rocketbudget.onrender.com/graphql"'
-        : '"http://localhost:4000/graphql"'
-    }
-  };
-});
+export default defineConfig({
+  plugins: [react()],
+})
