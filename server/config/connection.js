318880492035f82e3e37f44ce39f022287863864
Justbeingtai <taiho909pro@gmail.com>
2024-11-05 23:30:15 -0600

    trying to figure out render

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,16 +4,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connectOptions = {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  tls: true,
-  tlsAllowInvalidCertificates: true,
-};
-
-mongoose.connect(process.env.MONGODB_URI, connectOptions);
+});
 
 const db = mongoose.connection;
+
 db.on('error', (error) => console.error('MongoDB connection error:', error));
 db.once('open', () => {
   console.log('Connected to MongoDB');
