4b82f5b0cd08f3dd8354f2b39360e28879a92c35
Justbeingtai <taiho909pro@gmail.com>
2024-11-13 19:36:49 -0600

    updated back end

--- server/models/User.js
+++ server/models/User.js
@@ -1,23 +1,12 @@
+// server/models/User.js
 import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
-
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true }, // Keep the password field minimal
 });
 
-// Hash password before saving the user
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
-
-// Method to compare input password with hashed password in the database
-userSchema.methods.comparePassword = function (password) {
-  return bcrypt.compare(password, this.password);
-};
+// No additional hooks or methods here, as hashing is managed in the controller
 
 export default mongoose.model('User', userSchema);
